a7a0504f9c663ab8f325103f941cf29c2fbff430
sacchin-adarsh <sacchinadarsh@gmail.com>
2024-07-12 15:26:26 +0530

    Added printName method

--- src/main/java/com/training/bms/AlstromAppApplication.java
+++ src/main/java/com/training/bms/AlstromAppApplication.java
@@ -2,13 +2,21 @@ package com.training.bms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AlstromAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlstromAppApplication.class, args);
 		System.out.println("ALSTROM APP STARTED on SUNDAY");
+	}
+	
+	@GetMapping("happyWeekend")
+	public String printName() {
+		return "Happy weekend Sacchin ";
 	}
 
 }
